3c7fa06dd5ae5d47e302a18ca0ec5f822b8aa6fb
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-07-03 18:07:21 -1000

    resolves tests 1-2

--- multiples.js
+++ multiples.js
@@ -6,7 +6,18 @@
 exports.sumOfAMultiple = function( n ) {
   let sum = 0;
 
-  // do your work here
+  function multiples() {
+    let total = 0;
+    for (i = 0; i < n; i++) {
+      if (i % 3 === 0) {
+        total = total + i;
+      } else if (i % 5 === 0) {
+        total = total + i;
+      }
+    }
+    return total;
+  };
+  sum = multiples(n);
   
   return sum;
 };
